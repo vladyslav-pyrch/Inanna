d43accceb1512460317ad326f0f347380f3a65be
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-10-31 23:50:38 +0100

    Switched from Grpc to webapi

---
+++ src/LibraryContext/Api/Controllers/MangaController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inanna.LibraryContext.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MangaController : ControllerBase
+{
+    
+}
--- src/LibraryContext/Api/Program.cs
+++ src/LibraryContext/Api/Program.cs
@@ -1,42 +1,25 @@
-using Inanna.LibraryContext.Api.Services;
 using Inanna.LibraryContext.Application;
 using Inanna.LibraryContext.Infrastructure;
-using Inanna.LibraryContext.Infrastructure.DataAccess.Mangas.Projections;
-using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddGrpc().AddJsonTranscoding();
-builder.Services.AddGrpcSwagger();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
-    
-    string file = Path.Combine(AppContext.BaseDirectory, "Inanna.LibraryContext.Api.xml");
-    c.IncludeXmlComments(file);
-    c.IncludeGrpcXmlComments(file);
-});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
 builder.Services.AddInfrastructureModule();
 builder.Services.AddApplicationModule();
 
 WebApplication app = builder.Build();
 
-using (IServiceScope serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-{
-    var context = serviceScope.ServiceProvider.GetRequiredService<MangasProjectionsDbContext>();
-    context.Database.EnsureCreated();
-}
-
-app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<MangaService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.UseHttpsRedirection();
 
+app.MapControllers();
 app.Run();
